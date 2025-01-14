cc85bfcf20e90bf1367ba97b2f0d994d6b0427de
picantodriver <marcangeloarta.galan@bicol-u.edu.ph>
2025-01-14 14:56:13 +0800

    Added curriculum_id column in student (linked to curricula table)

---
+++ database/migrations/2025_01_14_054713_add_curriculum_id_to_students_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->foreignId('curriculum_id')->nullable()->references('id')->on('curriculas')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('curriculum_id');
+            $table->dropForeign(['curriculum_id']);
+        });
+    }
+};
